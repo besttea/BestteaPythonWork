a54b378a5bd940c8e8b7b573901f87985ca2929c
besttea <best-tea@163.com>
2021-09-10 01:31:57 +0800

    20200910第一课

---
+++ tiantian01.py
@@ -0,0 +1,34 @@
+#%%[markdown]
+'''
+Author: your name
+Date: 2021-09-08 20:39:56
+LastEditTime: 2021-09-08 23:47:48
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \0001\tiantian01.py
+'''
+#%%
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+#%%
+
+a = 1
+b = 1
+c = 1
+for a in range(100) :
+  for b in range(100):
+     for c in range(100):        
+        if b!=a and  b!=c:
+           d1=a*b*c 
+           d2=a+b+c 
+           if d1==d2 :
+              print('a*b*c=a+b+c', 'a=', a, 'b=', b, 'c=', c)
+
+
+# %%
+
+
+
