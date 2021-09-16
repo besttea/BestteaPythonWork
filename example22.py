6ed32e06872ccf36d4c40156cb6778fc30dcecb9
besttea <best-tea@163.com>
2021-09-16 16:36:57 +0800

    20210916

---
+++ example22.py
@@ -0,0 +1,17 @@
+'''
+Author: your name
+Date: 2021-09-13 17:42:36
+LastEditTime: 2021-09-13 17:58:30
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \0001\example22.py
+'''
+inputNumber = -12
+if (inputNumber > 0):
+    RealNumber = inputNumber 
+elif(inputNumber == 0 ):
+    RealNumber=inputNumber
+else:
+    RealNumber=-inputNumber
+
+print("绝对值:",RealNumber)
