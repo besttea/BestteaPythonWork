e0651f863f3b9751a4f89c2bd136bfd3083bc1d6
besttea <best-tea@163.com>
2021-09-23 17:18:40 +0800

    20210923第三课

---
+++ test-1.py
@@ -0,0 +1,15 @@
+'''
+Author: your name
+Date: 2021-09-23 16:57:37
+LastEditTime: 2021-09-23 16:59:49
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \0001\test-1.py
+'''
+#
+R= 10
+i, S = 1, 1
+while (i<=R):
+    S = S * i
+    i = i + 1
+print("累加求和",S)
