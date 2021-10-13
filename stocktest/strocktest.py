9fbe716f1ae987610cace43ec8feb19f50c8c758
besttea <best-tea@163.com>
2021-10-13 12:25:35 +0800

    20211013第四课

---
+++ stocktest/strocktest.py
@@ -0,0 +1,184 @@
+'''
+Author: your name
+Date: 2021-08-24 00:00:17
+LastEditTime: 2021-08-25 10:55:14
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \stocktest\strocktest.py
+'''
+
+import tushare as ts
+import tensorflow        as tf
+from numpy.random          import seed
+from tensorflow.keras.models          import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Bidirectional
+from sklearn.preprocessing import MinMaxScaler
+from sklearn               import metrics
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#gpus = tf.config.list_physical_devices("GPU")
+
+#if gpus:
+#    tf.config.experimental.set_memory_growth(gpus[0], True)  # 设置GPU显存用量按需使用
+#    tf.config.set_visible_devices([gpus[0]], "GPU")
+
+#
+#设置相关参数
+# 支持中文
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
+# 确保结果尽可能重现
+seed(1)
+tf.random.set_seed(1)
+
+# 设置相关参数
+n_timestamp = 40    # 时间戳
+n_epochs = 20    # 训练轮数
+# ====================================
+#      选择模型：
+#            1: 单层 LSTM
+#            2: 多层 LSTM
+#            3: 双向 LSTM
+# ====================================
+model_type = 1
+#加载数据
+#data = pd.read_csv('./datasets/SH600519.csv')  # 读取股票文件
+
+# 初始化pro接口
+pro = ts.pro_api('84a7d42ba53be1e345b15650d19116113087ad5440320b4c6b7b6c67')
+
+# 拉取数据
+data = pro.daily(**{
+    "ts_code": '600028.sh',
+    "trade_date": "",
+    "start_date": "",
+    "end_date": "",
+    "offset": "",
+    "limit": ""
+}, fields=[
+    "trade_date",
+    "open",
+    "close",
+    "high",
+    "low",
+    "vol",
+    "ts_code"
+])
+print(data)
+#
+"""
+前(2426-300=2126)天的开盘价作为训练集,后300天的开盘价作为测试集
+"""
+training_set = data.iloc[0:2426 - 300, 2:3]
+test_set = data.iloc[2426 - 300:, 2:3]
+#四、数据预处理
+#1.归一化
+#将数据归一化，范围是0到1
+sc = MinMaxScaler(feature_range=(0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+testing_set_scaled = sc.transform(test_set)
+#2.时间戳函数
+# 取前 n_timestamp 天的数据为 X；n_timestamp+1天数据为 Y。
+
+
+def data_split(sequence, n_timestamp):
+    X = []
+    y = []
+    for i in range(len(sequence)):
+        end_ix = i + n_timestamp
+
+        if end_ix > len(sequence)-1:
+            break
+
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return array(X), array(y)
+
+
+X_train, y_train = data_split(training_set_scaled, n_timestamp)
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+
+X_test, y_test = data_split(testing_set_scaled, n_timestamp)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+#五、构建模型
+# 建构 LSTM模型
+if model_type == 1:
+    # 单层 LSTM
+    model = Sequential()
+    model.add(LSTM(units=50, activation='relu',
+                   input_shape=(X_train.shape[1], 1)))
+    model.add(Dense(units=1))
+if model_type == 2:
+    # 多层 LSTM
+    model = Sequential()
+    model.add(LSTM(units=50, activation='relu', return_sequences=True,
+                   input_shape=(X_train.shape[1], 1)))
+    model.add(LSTM(units=50, activation='relu'))
+    model.add(Dense(1))
+if model_type == 3:
+    # 双向 LSTM
+    model = Sequential()
+    model.add(Bidirectional(LSTM(50, activation='relu'),
+                            input_shape=(X_train.shape[1], 1)))
+    model.add(Dense(1))
+
+model.summary()  # 输出模型结构
+#
+#六、激活模型
+# 该应用只观测loss数值，不观测准确率，所以删去metrics选项，一会在每个epoch迭代显示时只显示loss值
+model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
+              loss='mean_squared_error')  # 损失函数用均方误差
+#七、训练模型
+history = model.fit(X_train, y_train,
+                    batch_size=64,
+                    epochs=n_epochs,
+                    validation_data=(X_test, y_test),
+                    validation_freq=1)  # 测试的epoch间隔数
+
+model.summary()
+#八、结果可视化
+#1.绘制loss图
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Training and Validation Loss by K同学啊')
+plt.legend()
+plt.show()
+#2.预测
+predicted_stock_price = model.predict(
+    X_test)                        # 测试集输入模型进行预测
+predicted_stock_price = sc.inverse_transform(
+    predicted_stock_price)  # 对预测数据还原---从（0，1）反归一化到原始范围
+real_stock_price = sc.inverse_transform(y_test)  # 对真实数据还原---从（0，1）反归一化到原始范围
+
+# 画出真实数据和预测数据的对比曲线
+plt.plot(real_stock_price, color='red', label='Stock Price')
+plt.plot(predicted_stock_price, color='blue', label='Predicted Stock Price')
+plt.title('Stock Price Prediction by K同学啊')
+plt.xlabel('Time')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
+#3.评估
+"""
+MSE  ：均方误差    ----->  预测值减真实值求平方后求均值
+RMSE ：均方根误差  ----->  对均方误差开方
+MAE  ：平均绝对误差----->  预测值减真实值求绝对值后求均值
+R2   ：决定系数，可以简单理解为反映模型拟合优度的重要的统计量
+
+详细介绍可以参考文章：https://blog.csdn.net/qq_38251616/article/details/107997435
+"""
+MSE = metrics.mean_squared_error(predicted_stock_price, real_stock_price)
+RMSE = metrics.mean_squared_error(predicted_stock_price, real_stock_price)**0.5
+MAE = metrics.mean_absolute_error(predicted_stock_price, real_stock_price)
+R2 = metrics.r2_score(predicted_stock_price, real_stock_price)
+
+print('均方误差: %.5f' % MSE)
+print('均方根误差: %.5f' % RMSE)
+print('平均绝对误差: %.5f' % MAE)
+print('R2: %.5f' % R2)
